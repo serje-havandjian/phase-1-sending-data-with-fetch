111d7f3c49e07fe8a92d8192e087838671adfe3d
Serje <Serje.havandjian@gmail.com>
2022-05-12 09:59:56 -0700

    commit tree ff550008e0785116bd2446e201e7b60439cd8e51

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(testName, testEmail){
+    
+    return fetch("http://localhost:3000/users", {
+        method:"POST",
+        headers:{
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: testName,
+            email: testEmail,
+        })
+    })
+    .then(response => response.json())
+    .then(response =>{
+        getElementOnDOM(response)
+    })
+    .catch((error)=>{
+       document.body.innerHTML = error.message
+    })
+}
+
+submitData("Sam", "email")
+
+function getElementOnDOM(response){
+    const element = document.querySelector("body")
+    element.textContent = response.id
+}
